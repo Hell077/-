6b2344a6a9b27475d02394e22326daf45aad5b03
Alexey Ovchinnikov <gdhald4@gmail.com>
2024-03-21 15:46:40 +0500

    Добавьте файлы проекта.

---
+++ AFK TIMER/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading;
+
+class Program
+{
+    static void Main()
+    {
+        int seconds = 0;
+        int minutes = 0;
+        int hours = 0;
+
+        while (true)
+        {
+            seconds++;
+            Console.WriteLine($"Я сижу афк {hours} часов {minutes} минут {seconds} секунд");
+            Thread.Sleep(1000);
+
+            if (seconds >= 60)
+            {
+                minutes++;
+                seconds = 0;
+                Console.WriteLine($"Прошло: {minutes} минут");
+            }
+
+            if (minutes >= 60)
+            {
+                hours++;
+                minutes = 0;
+                Console.WriteLine($"Прошло: {hours} часов");
+            }
+        }
+    }
+}
